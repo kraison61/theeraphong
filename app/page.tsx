dedc3ef70589e39a859b916c969e9939cd1c0cd9
kraison <oun.cav@gmail.com>
2025-01-08 11:41:03 +0700

    v7

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <Content />
       <Services />
       <ServiceImages ImageDetails={ImageDetails} />
-      <h1>Hello, world v6</h1>
+      <h1>Hello, world v7</h1>
     </div>
   );
 }
